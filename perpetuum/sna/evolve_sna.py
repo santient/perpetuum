1d5496964d55566fb7d4692d5cf757ee33c706cb
Santiago Benoit <santib27@gmail.com>
2019-12-18 22:06:51 -0500

    add prune rate

--- perpetuum/sna/evolve_sna.py
+++ perpetuum/sna/evolve_sna.py
@@ -7,10 +7,10 @@ class EvolvingSNA(SNA):
     def __init__(self, input_neurons, hidden_neurons, output_neurons, potential_decay=0.99, device=None):
         super().__init__(input_neurons, hidden_neurons, output_neurons, potential_decay, device)
 
-    def mutate(self, density, intensity):
+    def mutate(self, change_rate, change_intensity, prune_rate):
         sign = self.weights.sign()
-        delta = torch.empty_like(self.weights).uniform_(-intensity, intensity)
-        delta[torch.empty_like(self.weights).uniform_(0, 1) > density] = 0
+        delta = torch.empty_like(self.weights).uniform_(-change_intensity, change_intensity)
+        delta[torch.empty_like(self.weights).uniform_(0, 1) > change_rate] = 0
         self.weights += delta
-        self.weights[self.mask | (self.weights.sign() == -sign)] = 0
+        self.weights[self.mask | (self.weights.sign() == -sign) | (torch.empty_like(self.weights).uniform_(0, 1) <= prune_rate)] = 0
         self.weights.clamp_(-1, 1)
